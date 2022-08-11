3c9c993c6c6460ab3f2a9e3e588a6c289d675ff0
ycyogesh <ycyogesh183@gmail.com>
2022-08-11 13:29:53 +0530

    not finished......

--- sorting_an_array.js
+++ sorting_an_array.js
@@ -1,14 +1,13 @@
 var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ]
-var a,minj;
+var a,minj,v,e;
 var arr2 = []
 arr1.forEach((e,i) =>{
-    let min = e[i]
+    let min = e
     minj = i
-    arr1.forEach(v,j =>{
-    if(arr1[j]<min){
+    arr1.forEach((v,j) =>{
+    if(v<min){
         min = arr1[i]
         minj = j
-
     }})
     let t = arr1[i]
     arr1[i] = min
