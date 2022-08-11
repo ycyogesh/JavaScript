d64705f30141dbe8d917f0531ec5025ea0a0e43a
ycyogesh <ycyogesh183@gmail.com>
2022-08-11 14:53:08 +0530

    Frequency of a number

--- frequency_of_an_array.js
+++ frequency_of_an_array.js
@@ -1,11 +1,26 @@
 var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]
-var count = 0
+var temp = [],i = 0
+var count = 0,e;
 arr1.forEach(v => {
+    if(!found1(temp,v)){
     arr1.forEach(b =>{
         if(v==b){
-            count++
+            count++;
         } 
     })
-    console.log(v,count);
-    count = 0
-});
+    temp[i++] = v;
+    console.log(v,"-",count);
+    count = 0;
+    
+}});
+
+
+function found1(temp,v){
+    let found = false
+    temp.forEach(e=>{
+        if(v==e){
+            found = true;
+        }
+    });
+    return found;
+}
