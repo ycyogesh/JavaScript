5edaa0746fe0ef4d16f00de9d9b2cb634395b6aa
ycyogesh <ycyogesh183@gmail.com>
2022-08-11 15:49:00 +0530

    Swap case of character

--- swap_the_case_of_character.js
+++ swap_the_case_of_character.js
@@ -5,12 +5,11 @@ c = c.split("")
 var d
 d = c.forEach((a, i) => {
     if (a >= 'a' && a <= 'z') {
-        c[i] = a.toUpperCase();
+       return c[i] = a.toUpperCase();
     } else if (a >= 'A' && a <= 'Z') {
-        c[i] = a.toLowerCase();
+        return c[i] = a.toLowerCase();
     }else{
         return c[i] = c[i]
     }
-    return c
 });
 console.log(c.toString().replace(/,/g,""));
