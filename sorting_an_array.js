76063ed7b9b038b549efb1113651119c4dbc9cc7
ycyogesh <ycyogesh183@gmail.com>
2022-08-11 12:46:32 +0530

    not finished

--- sorting_an_array.js
+++ sorting_an_array.js
@@ -1,2 +1,15 @@
 var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ]
-
+let a
+var arr2 = []
+b = arr1.forEach(e =>{
+    if(e>a){
+        let t = e
+        e = a
+        a = t
+        // console.log(t);
+        arr2+=e
+    }else{
+        arr2+=e
+    }
+})
+console.log(arr2)
