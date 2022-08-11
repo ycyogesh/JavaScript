41773107fac60b0ca940d628e87dbff561476233
ycyogesh <ycyogesh183@gmail.com>
2022-08-11 23:47:02 +0530

    Find in Objects

---
+++ find_in_object.js
@@ -0,0 +1,15 @@
+value = [ { first: 'Elie', last: 'Schoppik', isCatOwner: true },
+  { first: 'Tim', last: 'Garcia', isCatOwner: true },
+  { first: 'Matt', last: 'Lane', isCatOwner: true },
+  { first: 'Colt', last: 'Steele', isCatOwner: true } ]
+
+var b = []
+var v;
+value.forEach(findInObj);
+console.log(b[0]);
+
+
+
+function findInObj(v,i,a){
+    return b[i] = v;
+}
