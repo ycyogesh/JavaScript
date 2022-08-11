cb97508a4623056085768792ae14f5844cd03720
ycyogesh <ycyogesh183@gmail.com>
2022-08-11 11:02:40 +0530

    map & filter

--- Sampled2.js
+++ Sampled2.js
@@ -8,16 +8,33 @@ for(i of a){
     console.log(i)
 }
 
+
+
 var sum = 1
 a.forEach(myfunction)
 
 function myfunction(v,i,a){
     console.log(v);
     sum = sum * v
+    console.log(this);
 }
-console.log(sum);
-*/
+//console.log(sum);
+
+
+
 
 a.forEach(function(v,i,a){
-    console.log(v);
+    //console.log(v);
+    console.log(this);
 })
+
+a.forEach((v)=> console.log(this));
+*/
+
+
+
+//console.log(a.map(e=>e+1))
+//console.log(a.filter(e=>e>2 && e<12))
+
+let b = a.filter(e=>e>2 && e<12)
+console.log(b);
