eb5251997022fe07c218060e0bd6f2d044d7aa78
ycyogesh <ycyogesh183@gmail.com>
2022-08-12 13:00:50 +0530

    Find by value

--- filter_by_value.js
+++ filter_by_value.js
@@ -1,11 +1,22 @@
 value = [{'first': 'Elie', 'last':"Schoppik"}, {'first': 'Tim', 'last':"Garcia", isCatOwner:
 true}, {'first': 'Matt', 'last':"Lane"}, {'first': 'Colt', 'last':"Steele", isCatOwner: true}]
 
-var v;
+var v,s,b;
+/*
 value.forEach(filterByValue);
 console.log(value);
 
 
 function filterByValue(v,i,a){
     return v['isCatOwner'] = true
+}
+*/
+filterByValue(value,'isCatOwner')
+console.log(b)
+
+
+
+function filterByValue(v,s){
+    b = v.filter(e=>e.hasOwnProperty([s]))
+    return b;
 }
