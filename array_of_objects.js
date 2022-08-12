7745384c5f1da9704638aa3536bcfd67344fa52d
ycyogesh <ycyogesh183@gmail.com>
2022-08-12 11:45:00 +0530

    Array of Objects

---
+++ array_of_objects.js
@@ -0,0 +1,144 @@
+college = [
+{
+    'name': 'yogesh', 
+    'dept': 'it', 
+    'id': 422518205051
+},
+{
+    'name': 'deva', 
+    'dept': 'it', 
+    'id': 422518205010
+},
+{
+    'name': 'suryapriya', 
+    'dept': 'it', 
+    'id': 422518205045
+}
+];
+employee = [{
+    'e_name' : 'yogesh',
+    'e.id' : 1,
+    'e_age' : 21
+},
+{
+    'e_name' : 'deva',
+    'e.id' : 2,
+    'e_age' : 21
+},
+{
+    'e_name' : 'surya',
+    'e.id' : 3,
+    'e_age' : 21
+}];
+company = [{
+    'c_name' : 'finstein',
+    'c_years' : 5,
+    'c_origin' : 'finconso'
+},
+{
+    'c_name' : 'finconso',
+    'c_years' : 2,
+    'c_origin' : 'finstein'
+},
+{
+    'c_name' : 'dteam',
+    'c_years' : 5,
+    'c_origin' : 'dcrew'
+}];
+friend = [{
+    'f_name' : 'deva',
+    'f_age' : 22,
+    'f_years' : 5
+},
+{
+    'f_name' : 'surya',
+    'f_age' : 22,
+    'f_years' : 2
+},
+{
+    'f_name' : 'stalin',
+    'f_age' : 25,
+    'f_years' : 1
+}]
+family = [{
+    'f_name' : 'chandrakasan',
+    'm_name' : 'santha',
+    'b_name' : 'vicky',
+    's_name' : 'uma'
+},
+{
+    'f_name' : 'rajendran',
+    'm_name' : 'devaki',
+    'b_name' : 'vicky',
+    's_name' : 'jayasuriya'
+},
+{
+    'f_name' : 'saravanan',
+    'm_name' : 'sathya',
+    'b_name' : 'stalin',
+    's_name' : 'subiksha'
+}];
+const newspapers = [
+    {
+        'name': 'thetimes',
+        'address': 'https://www.thetimes.co.uk/environment/climate-change'
+    },
+    {
+        'name': 'gaurdian',
+        'address': 'https://www.theguardian.com/environment/climate-crisis'
+    },
+    {
+        'name': 'telegraph',
+        'address': 'https://www.telegraph.co.uk/climate-change'
+    }
+];
+var events = [
+    {
+      'userId': 1,
+      'place': "Wormholes Allow Information to Escape Black Holes",
+      'name': "Check out this recent discovery about workholes",
+      'date': "2020-06-26T17:58:57.776Z",
+      'id': 1
+    },
+    {
+      'userId': 1,
+      'place': "Wormholes Allow Information to Escape Black Holes",
+      'name': "Check out this recent discovery about workholes",
+      'date': "2020-06-26T17:58:57.776Z",
+      'id': 2
+    },
+    {
+      'userId': 1,
+      'place': "Wormholes Allow Information to Escape Black Holes",
+      'name': "Check out this recent discovery about workholes",
+      'date': "2020-06-26T17:58:57.776Z",
+      'id': 3
+    }
+  ];
+value = [
+    { 'first': 'Elie', 'last': 'Schoppik', 'isCatOwner': true },
+    { 'first': 'Tim', 'last': 'Garcia', 'isCatOwner': true },
+    { 'first': 'Matt', 'last': 'Lane', 'isCatOwner': true },
+    { 'first': 'Colt', 'last': 'Steele', 'isCatOwner': true }
+];
+var library = [
+    {
+    'author': 'Bill Gates',
+    'title': 'The Road Ahead',
+    'readingStatus': true
+    },
+    {
+    'author': 'Steve Jobs',
+    'title': 'Walter Isaacson',
+    'readingStatus': true
+    },
+    {
+    'author': 'Suzanne Collins',
+    'title': 'Mockingjay: The Final Book of The Hunger Games',
+    'readingStatus': false}];
+
+names = [
+    {'first': 'Elie', 'last':"Schoppik"}, 
+    {'first': 'Tim', 'last':"Garcia"}, 
+    {'first': 'Matt', 'last':"Lane"}, 
+    {'first': 'Colt', 'last':"Steele"}]
