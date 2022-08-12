5ae0757f29949303fc0b54cccdb74e0bdd81ce88
ycyogesh <ycyogesh183@gmail.com>
2022-08-12 13:10:14 +0530

    Add key and value

--- add_key_and_value.js
+++ add_key_and_value.js
@@ -1,12 +1,12 @@
 names = [{'name': 'Elie'}, {'name': 'Tim'}, {'name': 'Matt'}, {'name': 'Colt'}]
 
 var v;
-addKeyAndValue(names,)
+addKeyAndValue(names)
 console.log(names);
 
 
 function addKeyAndValue(a){
     a.forEach(e=> {
-        return e['title'] = 'instructor'
+        e['title'] = 'instructor'
     });
 }
