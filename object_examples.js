4fb66ca6c39875d69dc3701abae6b9b692faa9c7
ycyogesh <ycyogesh183@gmail.com>
2022-08-12 10:08:35 +0530

    object creation

---
+++ object_examples.js
@@ -0,0 +1,12 @@
+yc = [{ 
+    'name': 'yogesh', 
+    'dept': 'it', 
+    'id': 422518205051, 
+    'age' : '21', 
+    'dob' : '20-12-2000', 
+    'address' : 'trcihy', 
+    'pincode' : 620014, 
+    'college' : 'ucev', 
+    'language' : 'python', 
+    'intersts' : 'python'
+}]
